43f7be71ca2e423a578e620e6357a90270e67c51
RajuSaiPainkra <rajusaipainkra007@gmail.com>
2025-08-15 07:25:38 +0530

    added text and Numerical Data, making some comment, and done typecasting

--- init.py
+++ init.py
@@ -1,1 +1,16 @@
 print(Hello, Echo)
+
+
+# Saving Number to Variables 
+
+## Numbers Data Type
+number1 = 10.20 # float Data type
+number2 = 30 # Integer Data type
+
+total = number1 + number2 # adding the int and float number 
+print(type(total)) # type casting and checking the data type of total if it is float or int
+
+text1 = "Olaf"
+text2 = "Elsa"
+text3 = "Anna"
+text4 = "Kristoff"
