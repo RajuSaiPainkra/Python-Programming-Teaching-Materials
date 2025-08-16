db04d8926814b11a35bb38b379137ded538712d9
RajuSaiPainkra <rajusaipainkra007@gmail.com>
2025-08-16 20:36:54 +0530

    Foundation Python Codes addes, used by every initial developer

---
+++ Hello Python.py
@@ -0,0 +1,12 @@
+# Foundational print snippet, used almost all Developers, Programmer
+
+# print() function - Built-In Function
+
+print("Hello, Python!") # this is Build-In function 
+
+# What if, we have to build the code with the Built-in Library
+
+import sys #import the sys library for printing the output
+
+sys.stdout.write("Hello, Python!") # using sys library to print the output without newline
+
