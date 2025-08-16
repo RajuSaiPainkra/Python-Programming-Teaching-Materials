c7df4d27b03bf806396d0f29055a26e6c625ac09
RajuSaiPainkra <rajusaipainkra007@gmail.com>
2025-08-16 20:46:51 +0530

    some more description about print function are added

--- Hello Python.py
+++ Hello Python.py
@@ -10,3 +10,26 @@ import sys #import the sys library for printing the output
 
 sys.stdout.write("Hello, Python!") # using sys library to print the output without newline
 
+
+## something more that i have been forget to learn : :
+
+# print is Keyword in python - a Reserved Keyword 
+# print() now it is Built-In Function in Python  ## function = a block of Code that only runs when it is called
+# print() is a function that prints the specified message to the screen, or other standard output
+
+# when we type - print("Hello, Python!")
+## it means we are Calling the Function print() with the argument "Hello, Python!"
+# 
+#  inside the print() function like "Hello, Python!" is called an Argument
+
+# Hello, Python! is a String Literal
+
+
+### method to print Hello 
+# print() function 
+# sys.stdout.write() function
+# f-String function
+
+
+# f string function 
+print(f"Hello, Python!") # using f-string to print the output
