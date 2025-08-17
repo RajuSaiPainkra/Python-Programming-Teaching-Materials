e157c9c74f6b8aa0aa9cfb8d1ff2724f6b5622f5
RajuSaiPainkra <rajusaipainkra007@gmail.com>
2025-08-17 07:14:18 +0530

    SyntaxError description are added

--- pyException.py
+++ pyException.py
@@ -9,3 +9,17 @@
 # TypeError - Occurs when an operation or function is applied to an object of inappropriate type
 # IndentationError - Occurs when there is an indentation error in the code
 # NameError - Occurs when a local or global name is not found
+
+
+
+## SyntaxError : does'n follow the Syntax Rules of Python
+        # with Opening or Closing Parenthesis, Brackets, or Quotes
+        # String Literal without Closing Quotes
+        # structure of the code is not correct
+
+# Example of SyntaxError
+# print("Hello, World!" # Missing closing parenthesis
+# print("Hello, world!) # Missing closing quote
+# print("Hello, 
+# World!") # Missing closing quote and parenthesis
+
