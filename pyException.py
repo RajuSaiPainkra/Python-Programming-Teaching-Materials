0c4fc93b925cc3e8915b56a5a46cc5c826125b91
RajuSaiPainkra <rajusaipainkra007@gmail.com>
2025-08-17 07:06:35 +0530

    Common Python exception and type are added to the file

---
+++ pyException.py
@@ -0,0 +1,11 @@
+# Python Exceptions
+# Exception Handling in Python
+
+# Exception Related to Execution and Getting Error during the # execution of the code
+
+# Most Common Exception in Python
+# ZeroDivisionError - Occurs when dividing by zero
+# SyntaxError - Occurs when there is a syntax error in the code
+# TypeError - Occurs when an operation or function is applied to an object of inappropriate type
+# IndentationError - Occurs when there is an indentation error in the code
+# NameError - Occurs when a local or global name is not found
