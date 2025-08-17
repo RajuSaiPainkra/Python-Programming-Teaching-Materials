92ccd7683eceea9bcc6961916244e58cf43ef946
RajuSaiPainkra <rajusaipainkra007@gmail.com>
2025-08-17 08:01:15 +0530

    Comments in python , description are trying to explain the python Comments

---
+++ comment.py
@@ -0,0 +1,25 @@
+# Comment 
+# Comment help to make Description of Codes
+# Comments used to transmit information to the reader of the code
+# comment used in KT Knowledge Transfer
+# Comments are not executed by the Python interpreter
+
+
+# Example of Comment
+## Single Line Comment
+    # This is a single line comment in Python
+
+
+
+## Multi-Line Comment
+    # This is a multi-line comment in Python
+    # This is a multi-line comment in Python
+    # This is a multi-line comment in Python
+    # This is a multi-line comment in Python
+
+
+## inline Comment
+print("Hello, Python!") # This is an inline comment in Python
+print("Olo, World!")
+
+
