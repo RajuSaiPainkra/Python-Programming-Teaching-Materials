197873664936dcd5a0fd520edefbebae6897d43a
RajuSaiPainkra <rajusaipainkra007@gmail.com>
2025-08-18 10:29:37 +0530

    sum of number using the def function: small code of line

---
+++ def_sum.py
@@ -0,0 +1,11 @@
+def sum(num1, num2):
+    sum = num1 + num2
+    return sum
+
+
+# input the values from users : 
+
+num1 = float(input("Enter the first Number : "))
+num2 = float(input("Enter the second Number : "))
+print(sum(num1, num2))
+
