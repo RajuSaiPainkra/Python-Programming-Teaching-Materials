895e000ec2f93beb7b5a81bc61737ff4f56589a3
RajuSaiPainkra <rajusaipainkra007@gmail.com>
2025-08-25 07:52:47 +0530

    code rewrite with day and examination respective

--- match_case.py
+++ match_case.py
@@ -1,6 +1,6 @@
 # match case statments Check
 
-
+"""
 def weekday(day):
     match day:
         case 0 : return "Sunday"
@@ -13,3 +13,20 @@ def weekday(day):
         case _ : return "Enter Valid Day Number" 
 
 print(weekday(1))
+"""
+
+# rewrite the code the check subject Examination
+
+def days(day):
+    match day:
+        case "Monday" : return "English Philosophy"
+        case "Tuesday" : return "Mathematics"
+        case "Wednesday" : return "Science"
+        case "Thursday" : return "Social Studies"
+        case "Friday" : return "Computer Science and Infomation Technology"
+        case "Saturday" : return "Economics and Monetry Studies"
+        case "Sunday" : return "Holiday"
+        case _ : return "Enter Valid Day Name "
+
+print(days("Monday"))
+print(days("sunday"))
